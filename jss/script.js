e650fb8add7aef4554bb72c3f5ee5e970b1ff473
Klaudija✧˖*°࿐ <89772847+klaudij@users.noreply.github.com>
2021-10-30 12:23:22 +0200

    Add files via upload

--- jss/script.js
+++ jss/script.js
@@ -19,3 +19,51 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+
+
+
+var lightDark = document.getElementById("lightDarkMode");
+var mobilee = document.getElementById("mobile");
+var desktopp = document.getElementById('desktop');
+
+
+// if(localStorage.getItem("theme")== null){
+//   localStorage.setItem("theme", "dark")
+// }
+
+
+
+// let localData = localStorage.getItem('theme');
+
+// if(localData == "dark"){
+//   lightDark.src = "img/sun.png";
+//   mobilee.src = "img/Group34.svg";
+//   desktopp.src = "img/Group35.svg";
+//   document.body.classList.remove("light-mode");
+// }else if(localData == "light"){
+//   lightDark.src = "img/sun.png";
+//   mobilee.src = "img/Group34.svg";
+//   desktopp.src = "img/Group35.svg";
+//   document.body.classList.add("light-mode")
+// }
+
+
+lightDark.onclick = function(){
+  document.body.classList.toggle("light-mode");
+  if(document.body.classList.contains("light-mode")){
+    lightDark.src = "img/moon.png";
+    mobilee.src = "img/Group34Light.svg";
+    desktopp.src = "img/Group35Light.svg";
+
+    localStorage.setItem("theme", "light")
+  }else {
+    lightDark.src = "img/sun.png";
+    mobilee.src = "img/Group34.svg";
+    desktopp.src = "img/Group35.svg";
+    localStorage.setItem("theme", "dark")
+  }
+
+  
+}
